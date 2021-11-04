4a2bc5e4b82614bc4317dd69ca8defaf24fec08e
rogerpuig <rogerpuigdemasa@gmail.com>
2021-11-04 14:21:32 +0100

    fusion js

--- src/assets/main.js
+++ src/assets/main.js
@@ -492,3 +492,174 @@ function del(event){
     envio.innerText= env+"€"
     total.innerText= subtot + env +"€"
 }
+
+
+// iPHONE 13
+
+// color circles
+
+const iPinkCircle = document.getElementById( 'i13Pcircle' );
+const iBlueCircle = document.getElementById( 'i13Bcircle' );
+
+//  variables
+
+let i13Front = document.getElementById( 'iP13BF' );
+let i13Side = document.getElementById( 'iP13BS' );
+let i13Center = document.getElementById( 'i13Center' );
+
+// small images clicked display in center
+
+i13Side.addEventListener( 'click', () => {
+    i13Center.src = i13Side.src;
+} );
+
+i13Front.addEventListener( 'click', () => {
+    i13Center.src = i13Front.src;
+} );
+
+//  CHOOSE COLORS
+
+// 13 pink
+
+iPinkCircle.addEventListener( 'click', ( e ) => {
+
+    i13Front.src = "src/assets/img/iphone13-pink.png";
+    i13Side.src = 'src/assets/img/iphone-13-pink-side.jpeg';
+    i13Center.src = "src/assets/img/iphone13-pink.png";
+
+} );
+
+// 13 blue
+
+iBlueCircle.addEventListener( 'click', ( e ) => {
+
+    i13Front.src = "src/assets/img/iphone13-blue.png";
+    i13Side.src = 'src/assets/img/iphone13-blue-side.png';
+    i13Center.src = "src/assets/img/iphone13-blue.png";
+
+} );
+
+// GIGA BYTES CARDS iPHONE 13
+
+const card128 = document.getElementById( '128' );
+const card256 = document.getElementById( '256' );
+const card512 = document.getElementById( '512' );
+const subprice = document.getElementById( 'subprice' );
+
+var total = 0;
+var subpriceTotal = 909;
+
+
+card128.addEventListener( 'click', () => {
+    let card128Price = 699;
+    total = card128Price + subpriceTotal;
+    total = total.toFixed( 2 );
+    subprice.innerHTML = total + '€';
+} );
+
+card256.addEventListener( 'click', () => {
+    let card256Price = 799;
+    total = card256Price + subpriceTotal;
+    total = total.toFixed( 2 );
+    subprice.innerHTML = total + '€';
+} );
+
+card512.addEventListener( 'click', () => {
+    let card512Price = 999;
+    total = card512Price + subpriceTotal;
+    total = total.toFixed( 2 );
+    subprice.innerHTML = total + '€';
+} );
+
+// BUTTONS CONTINUE & GO TO CART iPHONE 13
+
+const continueBtn = document.getElementById( 'continueBtn' );
+const goToCart = document.getElementById( 'goToCartBtn' );
+
+
+
+
+//----------!!!!    iPHONE 13 PRO  !!!!--------
+
+// color circles PRO
+
+const proGraffitiCircle = document.getElementById( 'proGcircle' );
+const proBlueCircle = document.getElementById( 'proBcircle' );
+
+//  variables PRO
+
+let pro13Front = document.getElementById( 'pro13GF' );
+let pro13Side = document.getElementById( 'pro13GS' );
+let pro13Center = document.getElementById( 'pro13GC' );
+
+// small images clicked display in center PRO
+
+pro13Side.addEventListener( 'click', () => {
+    pro13Center.src = pro13Side.src;
+} );
+
+pro13Front.addEventListener( 'click', () => {
+    pro13Center.src = pro13Front.src;
+} );
+
+//  CHOOSE COLOR PRO
+
+//  Graffiti PRO
+
+proGraffitiCircle.addEventListener( 'click', ( e ) => {
+
+    pro13Front.src = "src/assets/img/iphone13-pro-graffiti.png";
+    pro13Side.src = 'src/assets/img/iphone-13-pro-graffiti-side.png';
+    pro13Center.src = "src/assets/img/iphone13-pro-graffiti.png";
+
+} );
+
+// Blue PRO
+
+proBlueCircle.addEventListener( 'click', ( e ) => {
+
+    pro13Front.src = "src/assets/img/iphone13-pro-blue.png";
+    pro13Side.src = 'src/assets/img/iphone-13-pro-blue-side.jpeg';
+    pro13Center.src = "src/assets/img/iphone13-pro-blue.png";
+
+} );
+
+
+
+
+// GIGA BYTES CARDS iPHONE 13 PRO
+
+const card128Pro = document.getElementById( '128Pro' );
+const card256Pro = document.getElementById( '256Pro' );
+const card512Pro = document.getElementById( '512Pro' );
+const subpricePro = document.getElementById( 'subpricePro' );
+
+var totalPro = 0;
+var subpriceTotalPro = 999;
+
+// Card 128 Pro
+card128Pro.addEventListener( 'click', () => {
+    let card128PricePro = 699;
+    totalPro = card128PricePro + subpriceTotalPro;
+    totalPro = totalPro.toFixed( 2 );
+    subpricePro.innerHTML = totalPro + '€';
+} );
+// Card 256 Pro
+card256Pro.addEventListener( 'click', () => {
+    let card256PricePro = 799;
+    totalPro = card256PricePro + subpriceTotalPro;
+    totalPro = totalPro.toFixed( 2 );
+    subpricePro.innerHTML = totalPro + '€';
+} );
+// Card 512 Pro
+card512Pro.addEventListener( 'click', () => {
+    let card512PricePro = 999;
+    totalPro = card512PricePro + subpriceTotalPro;
+    totalPro = totalPro.toFixed( 2 );
+    subpricePro.innerHTML = totalPro + '€';
+} );
+
+// BUTTONS CONTINUE & GO TO CART PRO
+
+const continueBtnPro = document.getElementById('continueBtnPro');
+const goToCartPro = document.getElementById('goToCartBtnPro');
