ba4f75530190aa72f0ad3997b445ea1b97db28f1
GariHV <gariherrero@gmail.com>
2021-11-04 13:24:42 +0100

    profile update and img fix

--- src/assets/main.js
+++ src/assets/main.js
@@ -266,11 +266,9 @@ function case2ChangeThirdToMain() {
 /* Password input check */
 function CheckPassword(inputtxt) {
     var decimal = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
-    if (inputtxt.value.match(decimal)) {
-        alert('Correct, try another...')
+    if (inputtxt.value.match(decimal) && inputtxt.length >= 8 && inputtxt.length <= 20) {
         return true;
     } else {
-        alert('Wrong...!')
         return false;
     }
 }
