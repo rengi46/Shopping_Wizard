04883d1501b320da9b71b171430f0d6bf83cabfe
MarcelDurganteDev <info@marceldurgante.dev>
2021-11-04 13:34:14 +0100

    Javascript images change, GB cards, price subtotal and total done

--- marcel.js
+++ marcel.js
@@ -1,6 +1,6 @@
-// iPhone 13
+// iPHONE 13
 
-// color buttons
+// color circles
 
 const iPinkCircle = document.getElementById( 'i13Pcircle' );
 const iBlueCircle = document.getElementById( 'i13Bcircle' );
@@ -21,7 +21,9 @@ i13Front.addEventListener( 'click', () => {
     i13Center.src = i13Front.src;
 } );
 
-//  chose and change colors
+//  CHOOSE COLORS
+
+// 13 pink
 
 iPinkCircle.addEventListener( 'click', ( e ) => {
 
@@ -31,6 +33,7 @@ iPinkCircle.addEventListener( 'click', ( e ) => {
 
 } );
 
+// 13 blue
 
 iBlueCircle.addEventListener( 'click', ( e ) => {
 
@@ -40,21 +43,60 @@ iBlueCircle.addEventListener( 'click', ( e ) => {
 
 } );
 
+// GIGA BYTES CARDS iPHONE 13
 
-// iPhone 13 Pro
+const card128 = document.getElementById( '128' );
+const card256 = document.getElementById( '256' );
+const card512 = document.getElementById( '512' );
+const subprice = document.getElementById( 'subprice' );
 
-// color buttons
+var total = 0;
+var subpriceTotal = 909;
+
+
+card128.addEventListener( 'click', () => {
+    let card128Price = 699;
+    total = card128Price + subpriceTotal;
+    total = total.toFixed( 2 );
+    subprice.innerHTML = total + '€';
+} );
+
+card256.addEventListener( 'click', () => {
+    let card256Price = 799;
+    total = card256Price + subpriceTotal;
+    total = total.toFixed( 2 );
+    subprice.innerHTML = total + '€';
+} );
+
+card512.addEventListener( 'click', () => {
+    let card512Price = 999;
+    total = card512Price + subpriceTotal;
+    total = total.toFixed( 2 );
+    subprice.innerHTML = total + '€';
+} );
+
+// BUTTONS CONTINUE & GO TO CART iPHONE 13
+
+const continueBtn = document.getElementById( 'continueBtn' );
+const goToCart = document.getElementById( 'goToCartBtn' );
+
+
+
+
+//----------!!!!    iPHONE 13 PRO  !!!!--------
+
+// color circles PRO
 
 const proGraffitiCircle = document.getElementById( 'proGcircle' );
 const proBlueCircle = document.getElementById( 'proBcircle' );
 
-//  variables
+//  variables PRO
 
 let pro13Front = document.getElementById( 'pro13GF' );
 let pro13Side = document.getElementById( 'pro13GS' );
 let pro13Center = document.getElementById( 'pro13GC' );
 
-// small images clicked display in center
+// small images clicked display in center PRO
 
 pro13Side.addEventListener( 'click', () => {
     pro13Center.src = pro13Side.src;
@@ -64,7 +106,9 @@ pro13Front.addEventListener( 'click', () => {
     pro13Center.src = pro13Front.src;
 } );
 
-//  chose and change colors
+//  CHOOSE COLOR PRO
+
+//  Graffiti PRO
 
 proGraffitiCircle.addEventListener( 'click', ( e ) => {
 
@@ -74,6 +118,7 @@ proGraffitiCircle.addEventListener( 'click', ( e ) => {
 
 } );
 
+// Blue PRO
 
 proBlueCircle.addEventListener( 'click', ( e ) => {
 
@@ -83,6 +128,44 @@ proBlueCircle.addEventListener( 'click', ( e ) => {
 
 } );
 
-// iPhone 13 Pro Blue
+
+
+
+// GIGA BYTES CARDS iPHONE 13 PRO
+
+const card128Pro = document.getElementById( '128Pro' );
+const card256Pro = document.getElementById( '256Pro' );
+const card512Pro = document.getElementById( '512Pro' );
+const subpricePro = document.getElementById( 'subpricePro' );
+
+var totalPro = 0;
+var subpriceTotalPro = 999;
+
+// Card 128 Pro
+card128Pro.addEventListener( 'click', () => {
+    let card128PricePro = 699;
+    totalPro = card128PricePro + subpriceTotalPro;
+    totalPro = totalPro.toFixed( 2 );
+    subpricePro.innerHTML = totalPro + '€';
+} );
+// Card 256 Pro
+card256Pro.addEventListener( 'click', () => {
+    let card256PricePro = 799;
+    totalPro = card256PricePro + subpriceTotalPro;
+    totalPro = totalPro.toFixed( 2 );
+    subpricePro.innerHTML = totalPro + '€';
+} );
+// Card 512 Pro
+card512Pro.addEventListener( 'click', () => {
+    let card512PricePro = 999;
+    totalPro = card512PricePro + subpriceTotalPro;
+    totalPro = totalPro.toFixed( 2 );
+    subpricePro.innerHTML = totalPro + '€';
+} );
+
+// BUTTONS CONTINUE & GO TO CART PRO
+
+const continueBtnPro = document.getElementById('continueBtnPro');
+const goToCartPro = document.getElementById('goToCartBtnPro');
 
 
